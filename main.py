4e95febe2325d14efe489bc9ced06b7acc4b7430
ionvop <ionvop@gmail.com>
2024-09-12 15:47:04 +0800

    Update

---
+++ main.py
@@ -0,0 +1,53 @@
+import os
+import zipfile
+import time
+import shutil
+import datetime
+
+
+def main() -> None:
+    if os.path.exists("Project.sb3"):
+        if not os.path.isdir("project"):
+            update()
+
+        os.remove("Project.sb3")
+
+    zipf = zipfile.ZipFile("Project.sb3", "w")
+
+    for file in os.listdir("project/"):
+        path = os.path.join("project/", file)
+
+        if os.path.isdir(path):
+            continue
+
+        zipf.write(path, file)
+
+    zipf.close()
+    os.startfile("Project.sb3")
+    print(f"Loaded at {datetime.datetime.now()}")
+    last_modified = os.path.getmtime("Project.sb3")
+    
+    try:
+        while True:
+            if last_modified != os.path.getmtime("Project.sb3"):
+                update()
+                print(f"Updated at {datetime.datetime.now()}")
+                last_modified = os.path.getmtime("Project.sb3")
+
+            time.sleep(1)
+    except KeyboardInterrupt:
+        os.remove("Project.sb3")
+        print(f"Exited at {datetime.datetime.now()}")
+
+
+def update() -> None:
+    if os.path.isdir("project"):
+        shutil.rmtree("project")
+
+    zipf = zipfile.ZipFile("Project.sb3", "r")
+    zipf.extractall("project")
+    zipf.close()
+
+
+if __name__ == "__main__":
+    main()
